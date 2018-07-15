bbf429fa79d527f8817a3ec0be2efe27714c3ece
James Hartanto <jameshartanto1993@gmail.com>
2018-07-14 18:18:57 -0700

    flexbox

--- fantasy-dota-app/src/index.js
+++ fantasy-dota-app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '../css/teamList.css';
+import './css/teamList.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker';
